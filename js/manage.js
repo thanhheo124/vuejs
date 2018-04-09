baf286dc534577da74085dd38a644d9bb11b3546
thanhheo124 <thanhbk.it37@gmail.com>
2018-04-09 18:09:31 +0700

    a

--- js/manage.js
+++ js/manage.js
@@ -1,6 +1,23 @@
 new Vue ({
   el : '#tasks',
   data: {
-      tasks : [{body:"Thiết Kế Web",completed:false}]
+      tasks : [{body:"Thiết Kế Web",completed:false},
+              {body:"Học trực tuyến",completed:false}
+      ],
+      newTask: ''
+  },
+
+  methods: {
+    addTask : function(event){
+      event.preventDefault();
+      this.tasks.push({
+        body : this.newTask,
+        completed : false
+      });
+      this.newTask = '';
+    },
+    removeTask : function(task){
+      this.tasks.$delete(task);
+    }
   }
 });
